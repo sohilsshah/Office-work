9e13bfd6a424373af227ccfd9b1aabfecf3ec9cf
sohushah <shahsohil.9999@gmail.com>
2016-07-11 19:15:31 +0530

    scroll

---
+++ scroll-magic/Scripts/main.js
@@ -0,0 +1,58 @@
+﻿$(document).ready(function () {
+    
+    var ele = $('html');
+    var scroll = $(window).outerHeight();
+
+    $(document).keypress(function (event) {
+        var key = event.keyCode;
+        if (key == 40) {
+            scroll_down();
+        }
+        if (key == 38) {
+            scroll_up();
+        }
+
+    });
+
+    function scroll_down() {
+        ele.animate({
+            scrollTop: (ele.scrollTop() + scroll)
+        }, 3500)
+    }
+
+    function scroll_up() {
+        ele.animate({
+            scrollTop: (ele.scrollTop() - scroll)
+        }, 3500)
+    }
+
+    $('.scroller-btn-up').click(function () {
+        // Scroll the element up    
+        scroll_up();
+    });
+
+    $('.scroller-btn-down').click(function () {
+        scroll_down();
+    });
+
+    $('.scroller-btn-up, .scroller-btn-down').bind({
+        click: function (e) {
+            // Prevent the default click action
+            e.preventDefault();
+        }
+    });
+
+    var controller = new ScrollMagic.Controller();
+
+    var truckScene = new ScrollMagic.Scene({
+        triggerElement: '.parallax',
+        duration: '50%',
+        //triggerHook:'0.5'
+    })
+        .setTween(TweenMax.to('#truck', 1, {
+            left: '40%', ease: Power0.easeNone
+        }))
+        .addIndicators()
+        .addTo(controller);
+
+});
---
+++ scroll-magic/Scripts/scroll-amount.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    alert('hi');
+    var ele = $('html');
+    var scroll = 60;
+
+    $('.scroller-btn-up').click(function () {
+        // Scroll the element up    
+        ele.animate({
+            scrollTop : (ele.scrollTop() - scroll)
+        })
+    });
+
+    $('.scroller-btn-down').click(function () {
+        //alert('hi');
+        // Scroll the element down
+        ele.animate({
+            scrollTop: (ele.scrollTop() + scroll)
+        })
+    });
+
+    $('.scroller-btn-up, .scroller-btn-down').bind({
+        click: function (e) {
+            // Prevent the default click action
+            e.preventDefault();
+        }
+    });
+});
